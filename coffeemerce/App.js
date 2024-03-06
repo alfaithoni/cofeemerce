bcc1a09d8390d06ffd4691248fcca49384a4bf14
Muhammad Hanif Al Faithoni <et4w4zz@gmail.com>
2024-03-06 22:00:56 +0700

    feat App Icon & Splach screen

---
+++ coffeemerce/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hi! Welcome to Coffeemerce!</Text>
+      <StatusBar style="dark" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize:30,
+    fontWeight: 'bold',
+    color: '#4F3231',
+  },
+});
